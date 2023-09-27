e0bec2c944640ea56968ec127e396470df2d7570
Levi Schoonhoven <levi.schoonhoven@southeasttech.edu>
2023-09-27 11:34:40 -0500

    step 12

--- Models/MovieContext.cs
+++ Models/MovieContext.cs
@@ -8,7 +8,35 @@ namespace chpt4x1.Models
             : base(options) 
         { } 
 
-        public DbSet<Movie> Movies { get; set; }    
+        public DbSet<Movie> Movies { get; set; }
+
+        protected override void OnModelCreating(ModelBuilder modelBuilder)
+        {
+            modelBuilder.Entity<Movie>().HasData(
+                new Movie
+                {
+                    MovieId = 1,
+                    Name = "CasaBlanca",
+                    Year = 1942,
+                    Rating = 5
+                },
+                new Movie
+                {
+                    MovieId = 2,
+                    Name = "Wonder Woman",
+                    Year = 2017,
+                    Rating = 3
+                },
+                new Movie
+                {
+                    MovieId = 3,
+                    Name = "Moonstruck",
+                    Year = 1988,
+                    Rating = 4
+                }
+
+             );
+        }
 
     }
 }
---
+++ Startup.cs
@@ -0,0 +1,28 @@
+﻿//using Microsoft.AspNetCore.Builder;
+using Microsoft.Extensions.Hosting;
+using Microsoft.EntityFrameworkCore;
+using chpt4x1.Models;
+
+
+
+namespace chpt4x1
+{
+    public class Startup
+    {
+        public Startup(IConfiguration configuration)
+        {
+            Configuration = configuration;
+        }
+
+        public IConfiguration Configuration { get; }
+
+
+        public void ConfigurationServices(IServiceCollection services)
+        {
+            
+            services.AddDbContext<MovieContext>(options =>
+                options.UseSqlServer(
+                    Configuration.GetConnectionString("MovieContext")));
+        }
+    }
+}
